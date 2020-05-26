ce19dbb66405ffb6619bb1530660a4626b2ca11e
Marta Arenas Martínez <martilla311@gmail.com>
2020-05-26 13:24:28 +0200

    Formulario añadir usuarios - Actividad ver usuarios

--- App2/app/src/main/java/com/example/app2/Agregar_usuario.java
+++ App2/app/src/main/java/com/example/app2/Agregar_usuario.java
@@ -2,13 +2,37 @@ package com.example.app2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Agregar_usuario extends AppCompatActivity {
 
+    String user_n;
+    EditText et_usuario, et_clave, et_nombre, et_hora_e, et_hora_s;
+    String usuario, clave, nombre, hora_e, hora_s;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_usuario);
+
+        Intent intent = getIntent();
+        user_n = intent.getStringExtra("name");
+    }
+
+    public void guardar(View v) {
+        et_usuario = findViewById(R.id.et_usuario);
+        et_clave = findViewById(R.id.etclave);
+        et_nombre = findViewById(R.id.et_nombre);
+        et_hora_e = findViewById(R.id.et_hora_e);
+        et_hora_s = findViewById(R.id.et_hora_s);
+
+        usuario = et_usuario.getText().toString();
+        clave = et_clave.getText().toString();
+        nombre = et_nombre.getText().toString();
+        hora_e = et_hora_e.getText().toString();
+        hora_s = et_hora_s.getText().toString();
     }
 }
---
+++ App2/app/src/main/java/com/example/app2/Ver_usuarios.java
@@ -0,0 +1,15 @@
+package com.example.app2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ver_usuarios extends AppCompatActivity {
+
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_usuarios);
+    }
+}
