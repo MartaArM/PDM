a41d55b1717d09dfe429237bef3076130f193d4f
Marta Arenas Martínez <martilla311@gmail.com>
2020-05-27 11:08:36 +0200

    Comprobación de nombre de usuario al agregar

--- App2/app/src/main/java/com/example/app2/Agregar_usuario.java
+++ App2/app/src/main/java/com/example/app2/Agregar_usuario.java
@@ -3,6 +3,8 @@ package com.example.app2;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -43,12 +45,44 @@ public class Agregar_usuario extends AppCompatActivity {
         hora_e = et_hora_e.getText().toString();
         hora_s = et_hora_s.getText().toString();
 
-        Usuario us = new Usuario(usuario, clave, nombre, hora_e, hora_s, "");
-        db.usuarioDao().insert(us);
+        if (usuarioExiste(usuario)) {
+            mostrarMensaje("El nombre de usuario ya existe");
+        }
+        else {
+            Usuario us = new Usuario(usuario, clave, nombre, hora_e, hora_s, "");
+            db.usuarioDao().insert(us);
 
-        Intent intent = new Intent(this, Administracion.class);
-        intent.putExtra("name", user_n);
-        startActivity(intent);
-        finish();
+            mostrarMensaje("Usuario añadido con exito.");
+
+            Intent intent = new Intent(this, Administracion.class);
+            intent.putExtra("name", user_n);
+            startActivity(intent);
+            finish();
+        }
+
+    }
+
+    private boolean usuarioExiste(String nombre) {
+        boolean existe = false;
+
+        if (db.usuarioDao().getUsuarioNombreUsuario(nombre) != null) {
+            existe = true;
+        }
+
+        return existe;
+    }
+
+    private void mostrarMensaje(String mensaje){
+        AlertDialog.Builder builder = new AlertDialog.Builder(Agregar_usuario.this);
+
+        builder.setMessage(mensaje);
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
     }
 }
