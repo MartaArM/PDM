6cc7aed4c2056a793c3b24ba281fe166da42bc74
Marta Arenas Martínez <martilla311@gmail.com>
2020-06-16 09:50:47 +0200

    Actividad para ver notificaciones

--- App3/app/src/main/java/com/example/app3/Administracion.java
+++ App3/app/src/main/java/com/example/app3/Administracion.java
@@ -1,8 +1,12 @@
 package com.example.app3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.app3.Database.AppDatabase;
 
 public class Administracion extends AppCompatActivity {
 
@@ -11,4 +15,9 @@ public class Administracion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administracion);
     }
+
+    public void ver_notificaciones(View v) {
+
+    }
+
 }
