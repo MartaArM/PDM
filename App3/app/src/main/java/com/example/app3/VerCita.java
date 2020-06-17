204503ce1a3eebd62b4ffe7c890d391e274fa70b
Marta Arenas Martínez <martilla311@gmail.com>
2020-06-17 10:46:12 +0200

    Eliminar cita ok

--- App3/app/src/main/java/com/example/app3/VerCita.java
+++ App3/app/src/main/java/com/example/app3/VerCita.java
@@ -3,8 +3,11 @@ package com.example.app3;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.example.app3.Database.AppDatabase;
@@ -46,5 +49,48 @@ public class VerCita extends AppCompatActivity {
         tvhoraf.setText(c.getHora_fin());
         tvdescripcion.setText(c.getDescripcion());
         tvemail.setText(c.getEmail());
+    }
+
+    public void eliminarCita(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(VerCita.this);
+
+        builder.setMessage("¿Desea eliminar la cita?");
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                db.citaDao().deleteById(id);
+                dialog.dismiss();
+                mostrarMensajeCerrar("La cita ha sido eliminada.");
+            }
+        });
+
+
+
+        builder.show();
+    }
+
+    public void editarCita(View v){
+        
+    }
+    private void mostrarMensajeCerrar(String mensaje){
+        AlertDialog.Builder builder = new AlertDialog.Builder(VerCita.this);
+
+        builder.setMessage(mensaje);
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                finish();
+            }
+        });
+
+        builder.show();
     }
 }
